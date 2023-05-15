5ee072236a724af1e55938387c446d2ead12dcb9
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-05-15 15:18:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
-
+let arr = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
+let updatedList = arr.map((element) => {
+// ["a", "some", "one"]
+let words = element.split(" ") ;
+let updatedString = words.reduce((prev, current, index) => {
+if(current != "The" && current != "an" && current != "a"){
+return prev + " " + current ;
+}
+return prev ;
+}, "")
+return updatedString ;
+}) ;
+let mp = {} ; 
+updatedList.forEach( (element, index) => {
+mp[element] = arr[index] ;
+});
+updatedList.sort();
+let finalAns = updatedList.map((element) => {
+return mp[element] ;
+})
+console.log(finalAns);
